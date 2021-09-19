e846cfe36cfe8292deea3dfb8de9b1225556990a
pablotoledo <jptgjuanpablo@gmail.com>
2021-09-19 11:59:12 +0200

    20

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 export default function Saludar(props) {
+    const {user, saludarFn} = props;
+    const {nombre, edad, color} = user;
+
+    console.log(user)
+    console.log(saludarFn)
 
     console.log(props);
 
     return(
         <div>
             <h2>
-                Hola {props.user.nombre}, tiene {props.user.edad} años y su color preferido es el {props.user.color}
+                Hola {nombre}, tiene {edad} años y su color preferido es el {color}
             </h2>
-            <button onClick={() => props.saludarFn(props.user.nombre)}>Saludar</button>
+            <button onClick={() => saludarFn(nombre)}>Saludar</button>
         </div>
     )
 
